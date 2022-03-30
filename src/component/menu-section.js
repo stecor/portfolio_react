13f02ee48caed4a5318cb32cd3ad5ced750be9a7
Stefano Corra <stefano.corra@hotmail.com>
2022-03-30 13:58:30 -0400

    data commit

--- src/component/menu-section.js
+++ src/component/menu-section.js
@@ -1,51 +1,45 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
-class MenuSection extends Component {
-  constructor(props) {
-    super(props)
-    this.toggle = 0
-  }
-
-  menu = () => {
-    if (this.toggle === 0) {
+const MenuSection = (props) => {
+  const [toggle, setToggle] = useState(0)
+  const menu = () => {
+    if (toggle === 0) {
       document
         .getElementById('menu-burguer')
         .setAttribute('id', 'menu-burguer-active')
-      this.toggle = 1
+      setToggle(1)
     } else {
       document
         .getElementById('menu-burguer-active')
         .setAttribute('id', 'menu-burguer')
-      this.toggle = 0
+      setToggle(0)
     }
   }
 
-  render() {
-    return (
-      <div id='menu-burguer' onClick={this.menu}>
-        <div className='menu-icon'>
-          <i className='fa fa-bars' aria-hidden='true'></i>
-        </div>
-        <section id='menu'>
-          <nav>
-            <Link to={'./'}>
-              <span>Home</span>
-            </Link>
-            <Link to={'./'}>
-              <span>About</span>
-            </Link>
-            <Link to={'./portfolio'}>
-              <span>Portfolio</span>
-            </Link>
-            <Link to={'./contact'}>
-              <span>Contact</span>t
-            </Link>
-          </nav>
-        </section>
+  return (
+    <div id='menu-burguer' onClick={menu}>
+      <div className='menu-icon'>
+        <i className='fa fa-bars' aria-hidden='true'></i>
       </div>
-    )
-  }
+      <section id='menu'>
+        <nav>
+          <Link to={'./'}>
+            <span>Home</span>
+          </Link>
+          <Link to={'./'}>
+            <span>About</span>
+          </Link>
+          <Link to={'./portfolio'}>
+            <span>Portfolio</span>
+          </Link>
+          <Link to={'./contact'}>
+            <span>Contact</span>t
+          </Link>
+        </nav>
+      </section>
+    </div>
+  )
 }
 
 export default MenuSection
