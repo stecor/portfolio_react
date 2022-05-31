2a2302905b80d6a7644990f6b8e6ceef31748cac
Stefano Corra <stefano.corra@hotmail.com>
2022-05-30 22:58:05 -0400

    fix profile

--- src/component/Profile.jsx
+++ src/component/Profile.jsx
@@ -23,11 +23,21 @@ const Profile = () => {
         <h1>
           Stefano <span>Corra</span>
         </h1>
-        <h5>FrontEnd Developer</h5>
+        <h5>Developer</h5>
         <p>
-          A proactive and organized individual with an Associate’s degree in
-          Computer Software and a background in developing applications in
-          searching for a Frontend position.
+          A proactive and organized individual with an Associate degree in
+          Computer Software and a background in web applications. As an
+          enthusiastic and creative technologist with hands-on skills in a wide
+          range of software, tools, and programming languages, positioned to
+          leverage my abilities and qualifications. Over the past 4 years, as a
+          Developer, I have designed, developed, and launched customized, highly
+          responsive websites and e-commerce solutions using advanced
+          technologies. In addition, I have consistently met client expectations
+          and project milestones while working effectively in deadline-driven
+          environments. Please consider the following highlights: Designed and
+          developed webpages using jQuery, JavaScript, HTML5, and CSS/Bootstrap,
+          Built components in ReactJS using asynchronous methods. Created
+          e-commerce sites for small business owners.
         </p>
 
         <Link
